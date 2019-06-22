5fa5fd440d8fe03762fccfb22dc4f518814462ee
sofianeMdjk <sofiane.mdjk@gmail.com>
2019-06-22 15:26:36 +0100

    Removed google drive management

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import sys
-
 from data import upload_dataset_partition
-from etc import DRIVE_INSTANCE_PATH
-from etc import settings
 from init_directories import init_directories
 from models import Seq2SeqModel
-from utils import load_pickle_data
 
 init_directories()
-settings.DRIVE_INSTANCE = load_pickle_data(DRIVE_INSTANCE_PATH)
 
 word_level = int(sys.argv[1])
 architecture = int(sys.argv[2])
@@ -19,4 +14,3 @@ upload_dataset_partition(word_level=word_level, partitions=64)
 model = Seq2SeqModel(latent_dim=latent_dim, epochs=epochs, model_architecture=architecture, word_level=word_level)
 model.train_model()
 
-# accuracy = measure_test_accuracy(test_decoder_input, model, encoder_states, latent_dim=512)
